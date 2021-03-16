ca112fd4b464a8afa3a59582b6ee083619151bbc
Frog-Has-Curls <64136125+Frog-Has-Curls@users.noreply.github.com>
2021-03-16 16:13:36 +0100

    Update zad1.pas

--- zad1.pas
+++ zad1.pas
@@ -1,21 +1,55 @@
+
 //bubble sort
-PROGRAM przykladowy;
+PROGRAM bubbleSort;
 
+CONST
+  n=100;
 VAR
-  stala=array[1..100] of Integer;
-  i,j,n: Integer;
-PROCEDURE sortuj
-  For i:=1 To n Do
-  Begin
+  stala: array[1..100] of Integer;
+  i,j,zm,temp: Integer;
 
-  End
-END
+
+PROCEDURE sortuj;
+ BEGIN
+  zm:=0;
+  While zm=0 Do
+    Begin
+       zm:=1;
+       For i:=1 To n-1 Do
+          Begin
+              if stala[i] > stala[i+1] then
+                  Begin
+                     zm:=0;
+                     temp := stala[i];
+                     stala[i] := stala[i+1];
+                     stala[i+1] := temp;
+
+                   End;
+
+          End;
+
+      End;
+   END;
+
+
+
+
 
 BEGIN
-
-  For I:1 To n Do
+  
+  For i:=1 To n Do
   Begin
-   stala[i] := Random(n)+1
-  End
-   sort()
+  
+   stala[i] := Random(n)+1;
+   write(' | ', stala[i] );
+  End;
+   sortuj();
+   writeln();
+   writeln(' Posortowane! ');
+   
+   for i:= 1 to n Do
+     Begin
+       write(' | ', stala[i] );
+     End;
+   
  END.

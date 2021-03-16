c15b3601d076fcbe243d049d2b95caca4864fcf7
Frog-Has-Curls <64136125+Frog-Has-Curls@users.noreply.github.com>
2021-03-16 13:54:29 +0100

    Create zad1.pas

---
+++ zad1.pas
@@ -0,0 +1,21 @@
+//bubble sort
+PROGRAM przykladowy;
+
+VAR
+  stala=array[1..100] of Integer;
+  i,j,n: Integer;
+PROCEDURE sortuj
+  For i:=1 To n Do
+  Begin
+
+  End
+END
+
+BEGIN
+
+  For I:1 To n Do
+  Begin
+   stala[i] := Random(n)+1
+  End
+   sort()
+ END.
